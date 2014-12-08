d764657e26ed48e39b37dc3d72ddb3fb22b9477b
Theo Fiedler <theo.fie@gmail.com>
2014-12-08 15:05:17 +0100

    added timeline four column support + test

--- test/chartkick_test.rb
+++ test/chartkick_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'date'
 
 class TestChartkick < Minitest::Test
   include Chartkick::Helper
@@ -6,7 +7,7 @@ class TestChartkick < Minitest::Test
   # TODO actual tests
 
   def setup
-    @data = [[34, 42], [56, 49]]
+    @data = [[34, 42], [56, 49]] 
   end
 
   def test_line_chart
@@ -21,6 +22,15 @@ class TestChartkick < Minitest::Test
     assert column_chart(@data)
   end
 
+  def test_timeline_chart
+    date_one = Date.new(2014,10,3)
+    date_two = Date.new(2014,11,3)
+    three_column_data = [['name', date_one, date_two]]
+    four_column_data = [['name','comment', date_one, date_two]]
+    assert column_chart(three_column_data)
+    assert column_chart(four_column_data)
+  end
+  
   def test_options_not_mutated
     options = {id: "boom"}
     line_chart @data, options
